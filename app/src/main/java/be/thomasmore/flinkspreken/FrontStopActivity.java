79648d9921da18542d87dfd16c63ea70ef93b3db
Bram Bleys <b.bleys@hotmail.com>
2018-11-15 12:35:10 +0100

    Generated content in popup instead op hard coded views

--- app/src/main/java/be/thomasmore/flinkspreken/FrontStopActivity.java
+++ app/src/main/java/be/thomasmore/flinkspreken/FrontStopActivity.java
@@ -4,10 +4,16 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
+
 
 public class FrontStopActivity extends AppCompatActivity {
 
@@ -24,6 +30,43 @@ public class FrontStopActivity extends AppCompatActivity {
         LayoutInflater inflater = this.getLayoutInflater();
 
         final View viewInflater = inflater.inflate(R.layout.info_popup, null);
+        LinearLayout first_layout = (LinearLayout) viewInflater.findViewById(R.id.first_layout);
+        LinearLayout second_layout = (LinearLayout) viewInflater.findViewById(R.id.second_layout);
+
+        LayoutParams layoutParams = new LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.topMargin = 16;
+        layoutParams.leftMargin = 4;
+        layoutParams.rightMargin = 4;
+        layoutParams.bottomMargin = 4;
+
+        TextView first_title = new TextView(this);
+        first_title.setText(R.string.fronting);
+        first_title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
+
+        TextView first_text = new TextView(this);
+        first_text.setText(R.string.info_text_fronting);
+
+
+        TextView second_title = new TextView(this);
+        second_title.setText(R.string.stopping);
+        second_title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
+
+        TextView second_text = new TextView(this);
+        second_text.setText(R.string.info_text_stopping);
+
+        first_title.setLayoutParams(layoutParams);
+        first_text.setLayoutParams(layoutParams);
+        second_title.setLayoutParams(layoutParams);
+        second_text.setLayoutParams(layoutParams);
+
+        first_layout.addView(first_title);
+        first_layout.addView(first_text);
+        second_layout.addView(second_title);
+        second_layout.addView(second_text);
+
+
         builder.setTitle(R.string.info_popup_title)
                 .setIcon(R.drawable.info_icon)
                 .setView(viewInflater)
