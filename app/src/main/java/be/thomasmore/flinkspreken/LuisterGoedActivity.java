b7dac152ee23820559433959414f6e218bf81e3b
Bram Bleys <b.bleys@hotmail.com>
2018-11-15 14:53:03 +0100

    Added clickListerner to button

---
+++ app/src/main/java/be/thomasmore/flinkspreken/LuisterGoedActivity.java
@@ -0,0 +1,13 @@
+package be.thomasmore.flinkspreken;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LuisterGoedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luister_goed);
+    }
+}
